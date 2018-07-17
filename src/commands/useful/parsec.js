3478933a80a106c6242ddde1c6120d15ab2239d4
Erik Nygren <sten.erik.nygren@gmail.com>
2018-07-17 13:23:42 -0400

    added parsec.js

---
+++ src/commands/useful/parsec.js
@@ -0,0 +1,29 @@
+import Promise from 'bluebird';
+import nconf from 'nconf';
+import R from 'ramda';
+import _request from 'request';
+
+
+const request = Promise.promisify(_request);
+
+function issueCommand(client, evt, suffix, lang) {
+  if (!nconf.get('PARSEC_KEY') || !nconf.get("PARSEC_BOT_URL")) return Promise.resolve("Parsec Key not set up");
+  const split_suffix = suffix.split(' ');
+  const cmd = split_suffix[0];
+
+  const options = {
+    url: nconf.get("PARSEC_BOT_URL"),
+    method: 'GET',
+    json: {
+      discord_user_id: evt.message.author.id,
+      cmd: cmd
+    }
+  };
+
+  return request(options).then(R.prop('msg'), R.prop("error"));
+}
+
+export default {
+  ping: issueCommand,
+  me: issueCommand,
+};
